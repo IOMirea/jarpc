b9b49e11a028ee23908f89e0767c08253d02ed0e
Eugene <gish.ee18@gmail.com>
2019-10-01 13:56:00 +0300

    Skip installing requirements, they are installed from setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setuptools.setup(
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
 )
