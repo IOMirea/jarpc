9a6fa9744235f133a3cf2c9cd79827edf1f4080d
physsi07 <starlinu33@gmail.com>
2019-10-03 18:15:53 -0400

    Fix: fixing duplicate code

--- tests/unit/enums_py/StatusCode_test.py
+++ tests/unit/enums_py/StatusCode_test.py
@@ -5,38 +5,29 @@ class enums_StatusCode_TestCase(unittest.TestCase):
     """
     Testing the StatusCode Class
     """
+    all_enumaration_types = [
+        StatusCode.SUCCESS,
+        StatusCode.BAD_FORMAT,
+        StatusCode.UNKNOWN_COMMAND,
+        StatusCode.BAD_PARAMS,
+        StatusCode.INTERNAL_ERROR
+    ]
 
     def test_enums_variables_values(self):
         """
         Should ensure that variables are set to their expected values
         """
-
-        all_enumarations_values = [
-            StatusCode.SUCCESS.value,
-            StatusCode.BAD_FORMAT.value,
-            StatusCode.UNKNOWN_COMMAND.value,
-            StatusCode.BAD_PARAMS.value,
-            StatusCode.INTERNAL_ERROR.value
-        ]
         
         enum_val = [0, 1, 2, 3, 4]
-        for enum in all_enumarations_values:
-            self.assertEqual(enum, enum_val[enum])
+        for enum in self.all_enumaration_types:
+            self.assertEqual(enum.value, enum_val[enum.value])
 
     def test_enums_type(self):
         """
         Should have the enum variable of type Enum
         """
 
-        all_enumaration_types = [
-            StatusCode.SUCCESS,
-            StatusCode.BAD_FORMAT,
-            StatusCode.UNKNOWN_COMMAND,
-            StatusCode.BAD_PARAMS,
-            StatusCode.INTERNAL_ERROR
-        ]
-
-        for enum in all_enumaration_types:
+        for enum in self.all_enumaration_types:
             self.assertEqual(type(enum), StatusCode)
 
 
