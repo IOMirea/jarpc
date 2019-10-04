bacdb28894f4da19fb9bbd973cea3993e12ed2db
physsi07 <starlinu33@gmail.com>
2019-10-03 23:53:39 -0400

    Travis: debugging pipeline

--- tests/unit/enums_py/StatusCode_test.py
+++ tests/unit/enums_py/StatusCode_test.py
@@ -1,37 +1,39 @@
 import unittest
 
-# from iomirea_rpc.enums import StatusCode
+from iomirea_rpc.enums import StatusCode
 
 
-# class enums_StatusCode_TestCase(unittest.TestCase):
-#     """
-#     Testing the StatusCode Class
-#     """
+class enums_StatusCode_TestCase(unittest.TestCase):
+    """
+    Testing the StatusCode Class
+    """
 
-#     all_enumaration_types = [
-#         StatusCode.SUCCESS,
-#         StatusCode.BAD_FORMAT,
-#         StatusCode.UNKNOWN_COMMAND,
-#         StatusCode.BAD_PARAMS,
-#         StatusCode.INTERNAL_ERROR,
-#     ]
+    print("Hey Travis")
 
-#     def test_enums_variables_values(self):
-#         """
-#         Should ensure that variables are set to their expected values
-#         """
+    # all_enumaration_types = [
+    #     StatusCode.SUCCESS,
+    #     StatusCode.BAD_FORMAT,
+    #     StatusCode.UNKNOWN_COMMAND,
+    #     StatusCode.BAD_PARAMS,
+    #     StatusCode.INTERNAL_ERROR,
+    # ]
 
-#         enum_val = [0, 1, 2, 3, 4]
-#         for enum in self.all_enumaration_types:
-#             self.assertEqual(enum.value, enum_val[enum.value])
+    # def test_enums_variables_values(self):
+    #     """
+    #     Should ensure that variables are set to their expected values
+    #     """
 
-#     def test_enums_type(self):
-#         """
-#         Should have the enum variable of type Enum
-#         """
+    #     enum_val = [0, 1, 2, 3, 4]
+    #     for enum in self.all_enumaration_types:
+    #         self.assertEqual(enum.value, enum_val[enum.value])
 
-#         for enum in self.all_enumaration_types:
-#             self.assertEqual(type(enum), StatusCode)
+    # def test_enums_type(self):
+    #     """
+    #     Should have the enum variable of type Enum
+    #     """
+
+    #     for enum in self.all_enumaration_types:
+    #         self.assertEqual(type(enum), StatusCode)
 
 
 # if __name__ == "__main__":
