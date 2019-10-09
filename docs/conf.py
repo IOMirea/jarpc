bbf0f6e3897d014b178b4c8de584f01d7da55707
Jens Reidel <jens@troet.org>
2019-10-09 19:52:10 +0200

    sphinx docs (#21)
    * First take at docs
    
    * Ignore _build
    
    * Now delete it :)
    
    * Move requirements file to docs/

---
+++ docs/conf.py
@@ -0,0 +1,81 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import os
+import re
+import sys
+import datetime
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+sys.path.insert(0, os.path.abspath(".."))
+
+
+# -- Project information -----------------------------------------------------
+
+year = datetime.date.today().year
+
+version = ""
+with open("../yarpc/__init__.py") as f:
+    cont = f.read()
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', cont, re.MULTILINE
+    ).group(1)
+    author = re.search(
+        r'^__author__\s*=\s*[\'"]([^\'"]*)[\'"]', cont, re.MULTILINE
+    ).group(1)
+
+project = "yarpc"
+copyright = f"{year}, {author}"
+
+# The full version, including alpha/beta/rc tags
+release = version
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinxcontrib.asyncio"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'alabaster'
+html_theme = "insegel"
+
+html_theme_options = {"wending_last_updated": False}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+# use index.rst
+master_doc = "index"
+source_suffix = ".rst"
+html_logo = "logo.png"
+html_favicon = "logo.png"
+
+html_last_updated_fmt = "%d %b %Y"
+releases_github_path = "IOMirea/yarpc"
+releases_unstable_prehistory = True
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import re
+
 from typing import List
 
 import setuptools
@@ -9,14 +11,27 @@ with open("requirements.txt") as f:
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+version = ""
+author = ""
+with open("yarpc/__init__.py") as f:
+    cont = f.read()
+    version_gr = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', cont, re.MULTILINE
+    )
+    if version_gr:
+        version = version_gr.group(1)
+    author_gr = re.search(r'^__author__\s*=\s*[\'"]([^\'"]*)[\'"]', cont, re.MULTILINE)
+    if author_gr:
+        author = author_gr.group(1)
+
 setuptools.setup(
-    name="iomirea_rpc",
-    version="0.2.0",
-    author="Eugene Ershov",
+    name="yarpc",
+    version=version,
+    author=author,
     description="RPC system used in IOMirea messenger",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/IOMirea/rpc",
+    url="https://github.com/IOMirea/yarpc",
     install_requires=requirements,
     python_requires=">=3.7.1",
     packages=setuptools.find_packages(),
